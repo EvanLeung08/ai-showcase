fe0becaf81a90b8fe4caad3e7d3dbe071c951a32
eshare <lyh10856214@qq.com>
2025-03-16 09:14:13 +0800

    refactor ImageGenerationService

--- src/main/java/com/evan/autoppt/service/ImageGenerationService.java
+++ src/main/java/com/evan/autoppt/service/ImageGenerationService.java
@@ -2,6 +2,7 @@ package com.evan.autoppt.service;
 
 import com.evan.autoppt.utils.MyUtil;
 import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.HttpUrl;
 import org.springframework.stereotype.Service;
 
@@ -12,26 +13,26 @@ import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+//讯飞大模型文生图像生成
+@Slf4j
 @Service
 public class ImageGenerationService {
     // 分离host和path定义
     public static final String hostUrl = "https://spark-api.cn-huabei-1.xf-yun.com/v2.1/tti";
-    private static final String API_PATH = "/v2.1/tti";
     public static final Gson gson = new Gson();
 
     public String generateImage(String prompt) throws Exception {
 
         String authUrl = getAuthUrl(hostUrl, System.getProperty("XF_API_KEY"), System.getProperty("XF_API_SECRET"));
         // URL地址正确
-        //System.err.println(authUrl);
+        log.info("鉴权URL，authUrl:{}", authUrl);
         String json = "{\n" + "  \"header\": {\n" + "    \"app_id\": \"" + System.getProperty("XF_APP_ID") + "\",\n" + "    \"uid\": \"" + UUID.randomUUID().toString().substring(0, 15) + "\"\n" + "  },\n" + "  \"parameter\": {\n" + "    \"chat\": {\n" + "      \"domain\": \"s291394db\",\n" + "      \"temperature\": 0.5,\n" + "      \"max_tokens\": 4096,\n" + "      \"width\": 1024,\n" + "      \"height\": 1024\n" + "    }\n" + "  },\n" + "  \"payload\": {\n" + "    \"message\": {\n" + "      \"text\": [\n" + "        {\n" + "          \"role\": \"user\",\n" + "          \"content\": \""+prompt+"\"\n" + "        }\n" + "      ]\n" + "    }\n" + "  }\n" + "}";
         // 发起Post请求
-        //System.err.println(json);
+        log.info("请求参数：{}", json);
         String res = MyUtil.doPostJson(authUrl, null, json);
-        System.out.println(res);
+        log.info("返回结果：{}", res);
         JsonParse jsonParse = gson.fromJson(res, JsonParse.class);
         byte[] imageBytes = Base64.getDecoder().decode(jsonParse.payload.choices.text.get(0).content);
-
 
         return Base64.getEncoder().encodeToString(imageBytes);
     }
@@ -56,7 +57,6 @@ public class ImageGenerationService {
         byte[] hexDigits = mac.doFinal(preStr.getBytes(StandardCharsets.UTF_8));
         // Base64加密
         String sha = Base64.getEncoder().encodeToString(hexDigits);
-        // System.err.println(sha);
         // 拼接
         String authorization = String.format("api_key=\"%s\", algorithm=\"%s\", headers=\"%s\", signature=\"%s\"", apiKey, "hmac-sha256", "host date request-line", sha);
         // 拼接地址
@@ -66,7 +66,6 @@ public class ImageGenerationService {
                 addQueryParameter("host", url.getHost()).//
                 build();
 
-        // System.err.println(httpUrl.toString());
         return httpUrl.toString();
     }
 
